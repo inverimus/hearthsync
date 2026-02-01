f83599b3a0a14bec64f15dfcc7245141c6771586
Michael Green <mgreen@inverim.us>
2026-02-01 10:01:15 -0500

    use default bg color instead of black

--- src/select.nim
+++ src/select.nim
@@ -12,16 +12,16 @@ proc userSelect*(addon: Addon, options: seq[string]): int {.gcsafe.} =
   while true:
     for i, option in options:
       if selected == i + 1:
-        t.write(13, addon.line + i + 1, bgWhite, fgBlack, &"{i + 1}: {option}", resetStyle)
+        t.write(13, addon.line + i + 1, bgWhite, fgDefault, &"{i + 1}: {option}", resetStyle)
       else:
-        t.write(13, addon.line + i + 1, bgBlack, fgWhite, &"{i + 1}: {option}", resetStyle)
+        t.write(13, addon.line + i + 1, bgDefault, fgWhite, &"{i + 1}: {option}", resetStyle)
     let newSelected = handleSelection(options.len, selected)
     if newSelected == selected:
       for i, option in options:
         if selected == i + 1:
-          t.write(13, addon.line + i + 1, bgBlack, fgGreen, &"{i + 1}: {option}", resetStyle)
+          t.write(13, addon.line + i + 1, bgDefault, fgGreen, &"{i + 1}: {option}", resetStyle)
         else:
-          t.write(13, addon.line + i + 1, bgBlack, styleDim, fgWhite, &"{i + 1}: {option}", resetStyle)
+          t.write(13, addon.line + i + 1, bgDefault, styleDim, fgWhite, &"{i + 1}: {option}", resetStyle)
       return selected - 1
     elif newSelected != -1:
       selected = newSelected
